3102ea75a633bb49e7a89685090108663c42027c
github username <github email address>
2019-09-16 14:25:49 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import shoppingListItemReducer from './reducers/shoppingListItemReducer';
-import App from './App';
-import './index.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux"; // I'm pretty sure this is what allows the rest of my application to have access to the store.
+// That's why i wrap the whole app component inside of this one so that when i pass the store to the provider then everything else will
+// have the prop
+import { createStore } from "redux"; // This is what allows me to use the react library to create my store with the two methods
+// get state and dispatch
+import shoppingListItemReducer from "./reducers/shoppingListItemReducer"; // This is my reduce, that, when called will change the state according
+// to what action was specified
+import App from "./App";
+import "./index.css";
+// I set the invocation of createStore = to a variable
 const store = createStore(
   shoppingListItemReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+// This is what will allow me to use the chrome extension.
 
 ReactDOM.render(
-  <App store={store}/>,
-  document.getElementById('root')
+  // This way my application will have access to the store...which also contains methods inside the store that can change the store itself
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
